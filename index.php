b17ee5d2185b69fd19ee01e222e0819fafbc7314
pao <mockismymain@gmail.com>
2023-08-09 19:52:56 -0300

    First Commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EAD</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+    
+    <section>
+        <h2>EAD Login</h2>
+        <form action="#">
+            <input type="text" name="usuario" class="campo" placeholder="Usuário" required>
+            <input type="password" name="senha" class="campo" placeholder="Senha" required>
+            <button>ENTRAR</button>
+        </form>
+        <p>ou</p>
+        <a href="#">Cadastre-Se</a>
+    </section>
+
+</body>
+</html>
